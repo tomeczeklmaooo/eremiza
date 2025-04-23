6287f1b614614415555a0dce0fd2fb2bafc8be35
tomeczeklmaooo <tomeczeklmao@proton.me>
2025-04-23 09:07:16 +0200

    report type in polish

--- scripts/new-report.js
+++ scripts/new-report.js
@@ -5,6 +5,7 @@ function send_report()
 	const inp_location_city = document.querySelector('#report-input-text-location-city').value;
 	const inp_location_phone = document.querySelector('#report-input-text-location-phone').value;
 	const inp_type = document.querySelector('#report-input-select-type').value;
+	let inp_type_pl = '';
 	const inp_description = document.querySelector('#report-input-textarea-description').value;
 
 	const post_regex = /^[0-9]{2}-[0-9]{3}$/.test(inp_location_post);
@@ -16,11 +17,37 @@ function send_report()
 	}
 
 	// also add to what unit it was reported to (from CSV)
+	switch (inp_type)
+	{
+		case 'car-accident':
+			inp_type_pl = 'Wypadek (samochodowy)';
+			break;
+		case 'accident':
+			inp_type_pl = 'Wypadek (ogólny)';
+			break;
+		case 'fire':
+			inp_type_pl = 'Pożar';
+			break;
+		case 'fallen-tree':
+			inp_type_pl = 'Powalone drzewo';
+			break;
+		case 'flooding':
+			inp_type_pl = 'Zalanie/powódź';
+			break;
+		case 'other':
+			inp_type_pl = 'Inne';
+			break;
+		case 'none':
+		default:
+			inp_type_pl = 'Nie wybrano rodzaju';
+			break;
+	}
+
 	let message = `
 	<strong>Wysłano zgłoszenie!</strong><br>
 	Lokalizacja: ${inp_location_address}, ${inp_location_post} ${inp_location_city}<br>
 	Numer kontaktowy: ${inp_location_phone}<br>
-	Rodzaj zgłoszenia: ${inp_type}<br>
+	Rodzaj zgłoszenia: ${inp_type_pl}<br>
 	Opis: ${inp_description}
 	`;
 
